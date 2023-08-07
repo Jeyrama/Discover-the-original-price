9f6a965fa9314dfb561c7a340ebae8d3a4e88acd
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-07 16:17:33 -0700

    Update Discover-the-original-price.js

--- Discover-the-original-price.js
+++ Discover-the-original-price.js
@@ -15,3 +15,7 @@ DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted 
 */
 
 // Solution
+
+function discoverOriginalPrice(discountedPrice, salePercentage){
+  return +(discountedPrice / (1 - salePercentage / 100)).toFixed(2);
+}
